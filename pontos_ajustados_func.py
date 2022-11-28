d09c4d5f877596fe99524a0e4e91428152490534
mariliatd <mariliatd@gmail.com>
2022-11-28 16:09:18 -0300

    adiciona novas demandas tratadas de funcionários e volta

---
+++ pontos_ajustados_func.py
@@ -0,0 +1,779 @@
+# dados filtrados dos arquivos ida_funcionario_butanta_8h.json e ida_funcionario_butanta_18h.json
+
+demanda_completa_func = {
+    "seg": {
+        480: {
+            "FEA": {
+                "8032": 73,
+                "8012": 39,
+            },
+            "Reitoria/Bancos": {
+                "8012": 179,
+                "8032": 62
+            },
+            "Geociências": {
+                "8022": 74,
+            },
+            "Biblioteca Brasiliana": {
+                "8012": 24,
+                "8032": 55
+            },
+            "Letras": {
+                "8022": 12,
+            },
+            "Portaria III": {
+                "8012": 49,
+                "8022": 67
+            },
+            "Biblioteca Farmácia e Química": {
+                "8022": 156
+            },
+            "Odontologia": {
+                "8012": 14
+            },
+            "Poli Biênio": {
+                "8032": 80,
+                "8012": 46
+            },
+            "Terminal Urbano": {
+                "8022": 19
+            },
+            "IPT": {
+                "8022": 19,
+            },
+            "Poli Civil": {
+                "8032": 19
+            },
+            "Poli Eletrotécnica": {
+                "8022": 94,
+            },
+            "Parada Matemática": {
+                "8012": 14,
+                "8022": 27
+            },
+            "Rua do Lago": {
+                "8022": 14
+            },
+            "Acesso CPTM I": {
+                "8022": 9
+            },
+            "Hospital Universitário": {
+                "8012": 405,
+                "8022": 176
+            },
+            "MAE": {
+                "8012": 58,
+            },
+            "FAU II": {
+                "8022": 37
+            },
+            "Cocesp I": {
+                "8012": 47,
+            },
+            "Raia Olímpica": {
+                "8022": 156,
+            },
+            "CRUSP": {
+                "8032": 31
+            },
+            "Reitoria": {
+                "8012": 93
+            },
+            "COPESP": {
+                "8022": 31
+            }
+        },
+        1080: {
+            "FEA": {
+                "8012": 35,
+                "8032": 13,
+            },
+            "Portaria III": {
+                "8012": 56,
+                "8022": 92
+            },
+            "Biblioteca Farmácia e Química": {
+                "8022": 71
+            },
+            "Poli Biênio": {
+                "8032": 71,
+                "8012": 29
+            },
+            "Odontologia": {
+                "8012": 14
+            },
+            "IPT": {
+                "8022": 18,
+            },
+            "Poli Civil": {
+                "8032": 18
+            },
+            "Butantan": {
+                "8022": 18
+            },
+            "Parada Matemática": {
+                "8012": 15,
+                "8022": 15
+            },
+            "Rua do Lago": {
+                "8022": 15
+            },
+            "Acesso CPTM I": {
+                "8022": 9
+            },
+            "Hospital Universitário": {
+                "8012": 508,
+                "8022": 160
+            },
+            "Reitoria/Bancos": {
+                "8012": 16,
+                "8032": 37
+            },
+            "MAE": {
+                "8012": 29
+            },
+            "Poli Eletrotécnica": {
+                "8022": 11
+            },
+            "FAU II": {
+                "8022": 12
+            },
+            "Cocesp I": {
+                "8012": 37
+            },
+            "Raia Olímpica": {
+                "8022": 73,
+            },
+            "CRUSP": {
+                "8032": 37
+            },
+            "Biblioteca Brasiliana": {
+                "8012": 15,
+                "8032": 52
+            },
+            "Reitoria": {
+                "8012": 37
+            }
+        }
+    },
+    "ter": {
+        480: {
+            "FEA": {
+                "8032": 73,
+                "8012": 26,
+            },
+            "Reitoria/Bancos": {
+                "8012": 179,
+                "8032": 62
+            },
+            "Geociências": {
+                "8022": 74,
+            },
+            "Biblioteca Brasiliana": {
+                "8012": 24,
+                "8032": 55
+            },
+            "Letras": {
+                "8022": 12,
+            },
+            "Portaria III": {
+                "8012": 49,
+                "8022": 67
+            },
+            "Biblioteca Farmácia e Química": {
+                "8022": 156
+            },
+            "Odontologia": {
+                "8012": 14
+            },
+            "Poli Biênio": {
+                "8032": 80,
+                "8012": 46
+            },
+            "Terminal Urbano": {
+                "8022": 19
+            },
+            "IPT": {
+                "8022": 19,
+            },
+            "Poli Civil": {
+                "8032": 19
+            },
+            "Poli Eletrotécnica": {
+                "8022": 94,
+            },
+            "Parada Matemática": {
+                "8012": 14,
+                "8022": 27
+            },
+            "Rua do Lago": {
+                "8022": 14
+            },
+            "Acesso CPTM I": {
+                "8022": 9
+            },
+            "Hospital Universitário": {
+                "8012": 405,
+                "8022": 176
+            },
+            "MAE": {
+                "8012": 58,
+            },
+            "FAU II": {
+                "8022": 37
+            },
+            "Cocesp I": {
+                "8012": 47,
+            },
+            "Raia Olímpica": {
+                "8022": 156,
+            },
+            "CRUSP": {
+                "8032": 31
+            },
+            "Reitoria": {
+                "8012": 93
+            },
+            "COPESP": {
+                "8022": 31
+            }
+        },
+        1080: {
+            "FEA": {
+                "8012": 35,
+                "8032": 13,
+            },
+            "Portaria III": {
+                "8012": 56,
+                "8022": 92
+            },
+            "Biblioteca Farmácia e Química": {
+                "8022": 71
+            },
+            "Poli Biênio": {
+                "8032": 71,
+                "8012": 29
+            },
+            "Odontologia": {
+                "8012": 14
+            },
+            "IPT": {
+                "8022": 18,
+            },
+            "Poli Civil": {
+                "8032": 18
+            },
+            "Butantan": {
+                "8022": 18
+            },
+            "Parada Matemática": {
+                "8012": 15,
+                "8022": 15
+            },
+            "Rua do Lago": {
+                "8022": 15
+            },
+            "Acesso CPTM I": {
+                "8022": 9
+            },
+            "Hospital Universitário": {
+                "8012": 508,
+                "8022": 160
+            },
+            "Reitoria/Bancos": {
+                "8012": 16,
+                "8032": 37
+            },
+            "MAE": {
+                "8012": 29
+            },
+            "Poli Eletrotécnica": {
+                "8022": 11
+            },
+            "FAU II": {
+                "8022": 12
+            },
+            "Cocesp I": {
+                "8012": 37
+            },
+            "Raia Olímpica": {
+                "8022": 73,
+            },
+            "CRUSP": {
+                "8032": 37
+            },
+            "Biblioteca Brasiliana": {
+                "8012": 15,
+                "8032": 52
+            },
+            "Reitoria": {
+                "8012": 37
+            }
+        }
+    },
+    "qua": {
+        480: {
+            "FEA": {
+                "8032": 73,
+                "8012": 39,
+            },
+            "Reitoria/Bancos": {
+                "8012": 179,
+                "8032": 62
+            },
+            "Geociências": {
+                "8022": 74,
+            },
+            "Biblioteca Brasiliana": {
+                "8012": 24,
+                "8032": 55
+            },
+            "Letras": {
+                "8022": 12,
+            },
+            "Portaria III": {
+                "8012": 49,
+                "8022": 67
+            },
+            "Biblioteca Farmácia e Química": {
+                "8022": 156
+            },
+            "Odontologia": {
+                "8012": 14
+            },
+            "Poli Biênio": {
+                "8032": 80,
+                "8012": 46
+            },
+            "Terminal Urbano": {
+                "8022": 19
+            },
+            "IPT": {
+                "8022": 19,
+            },
+            "Poli Civil": {
+                "8032": 19
+            },
+            "Poli Eletrotécnica": {
+                "8022": 94,
+            },
+            "Parada Matemática": {
+                "8012": 14,
+                "8022": 27
+            },
+            "Rua do Lago": {
+                "8022": 14
+            },
+            "Acesso CPTM I": {
+                "8022": 9
+            },
+            "Hospital Universitário": {
+                "8012": 405,
+                "8022": 176
+            },
+            "MAE": {
+                "8012": 58,
+            },
+            "FAU II": {
+                "8022": 37
+            },
+            "Cocesp I": {
+                "8012": 47,
+            },
+            "Raia Olímpica": {
+                "8022": 156,
+            },
+            "CRUSP": {
+                "8032": 31
+            },
+            "Reitoria": {
+                "8012": 93
+            },
+            "COPESP": {
+                "8022": 31
+            }
+        },
+        1080: {
+            "FEA": {
+                "8012": 35,
+                "8032": 13,
+            },
+            "Portaria III": {
+                "8012": 56,
+                "8022": 92
+            },
+            "Biblioteca Farmácia e Química": {
+                "8022": 71
+            },
+            "Poli Biênio": {
+                "8032": 71,
+                "8012": 29
+            },
+            "Odontologia": {
+                "8012": 14
+            },
+            "IPT": {
+                "8022": 18,
+            },
+            "Poli Civil": {
+                "8032": 18
+            },
+            "Butantan": {
+                "8022": 18
+            },
+            "Parada Matemática": {
+                "8012": 15,
+                "8022": 15
+            },
+            "Rua do Lago": {
+                "8022": 15
+            },
+            "Acesso CPTM I": {
+                "8022": 9
+            },
+            "Hospital Universitário": {
+                "8012": 508,
+                "8022": 160
+            },
+            "Reitoria/Bancos": {
+                "8012": 16,
+                "8032": 37
+            },
+            "MAE": {
+                "8012": 29
+            },
+            "Poli Eletrotécnica": {
+                "8022": 11
+            },
+            "FAU II": {
+                "8022": 12
+            },
+            "Cocesp I": {
+                "8012": 37
+            },
+            "Raia Olímpica": {
+                "8022": 73,
+            },
+            "CRUSP": {
+                "8032": 37
+            },
+            "Biblioteca Brasiliana": {
+                "8012": 15,
+                "8032": 52
+            },
+            "Reitoria": {
+                "8012": 37
+            }
+        }
+    },
+    "qui": {
+        480: {
+            "FEA": {
+                "8032": 73,
+                "8012": 39,
+            },
+            "Reitoria/Bancos": {
+                "8012": 179,
+                "8032": 62
+            },
+            "Geociências": {
+                "8022": 74,
+            },
+            "Biblioteca Brasiliana": {
+                "8012": 24,
+                "8032": 55
+            },
+            "Letras": {
+                "8022": 12,
+            },
+            "Portaria III": {
+                "8012": 49,
+                "8022": 67
+            },
+            "Biblioteca Farmácia e Química": {
+                "8022": 156
+            },
+            "Odontologia": {
+                "8012": 14
+            },
+            "Poli Biênio": {
+                "8032": 70,
+                "8012": 46
+            },
+            "Terminal Urbano": {
+                "8022": 19
+            },
+            "IPT": {
+                "8022": 19,
+            },
+            "Poli Civil": {
+                "8032": 19
+            },
+            "Poli Eletrotécnica": {
+                "8022": 94,
+            },
+            "Parada Matemática": {
+                "8012": 14,
+                "8022": 27
+            },
+            "Rua do Lago": {
+                "8022": 14
+            },
+            "Acesso CPTM I": {
+                "8022": 9
+            },
+            "Hospital Universitário": {
+                "8012": 405,
+                "8022": 176
+            },
+            "MAE": {
+                "8012": 58,
+            },
+            "FAU II": {
+                "8022": 37
+            },
+            "Cocesp I": {
+                "8012": 47,
+            },
+            "Raia Olímpica": {
+                "8022": 156,
+            },
+            "CRUSP": {
+                "8032": 31
+            },
+            "Reitoria": {
+                "8012": 93
+            },
+            "COPESP": {
+                "8022": 31
+            },
+        },
+        1080: {
+            "FEA": {
+                "8012": 35,
+                "8032": 13,
+            },
+            "Portaria III": {
+                "8012": 56,
+                "8022": 92
+            },
+            "Biblioteca Farmácia e Química": {
+                "8022": 71
+            },
+            "Poli Biênio": {
+                "8032": 71,
+                "8012": 29
+            },
+            "Odontologia": {
+                "8012": 14
+            },
+            "IPT": {
+                "8022": 18,
+            },
+            "Poli Civil": {
+                "8032": 18
+            },
+            "Butantan": {
+                "8022": 18
+            },
+            "Parada Matemática": {
+                "8012": 15,
+                "8022": 15
+            },
+            "Rua do Lago": {
+                "8022": 15
+            },
+            "Acesso CPTM I": {
+                "8022": 9
+            },
+            "Hospital Universitário": {
+                "8012": 508,
+                "8022": 160
+            },
+            "Reitoria/Bancos": {
+                "8012": 16,
+                "8032": 37
+            },
+            "MAE": {
+                "8012": 29
+            },
+            "Poli Eletrotécnica": {
+                "8022": 11
+            },
+            "FAU II": {
+                "8022": 12
+            },
+            "Cocesp I": {
+                "8012": 37
+            },
+            "Raia Olímpica": {
+                "8022": 73,
+            },
+            "CRUSP": {
+                "8032": 37
+            },
+            "Biblioteca Brasiliana": {
+                "8012": 15,
+                "8032": 52
+            },
+            "Reitoria": {
+                "8012": 37
+            }
+        }
+    },
+    "sex": {
+        480: {
+            "FEA": {
+                "8032": 73,
+                "8012": 39,
+            },
+            "Reitoria/Bancos": {
+                "8012": 179,
+                "8032": 62
+            },
+            "Geociências": {
+                "8022": 74,
+            },
+            "Biblioteca Brasiliana": {
+                "8012": 24,
+                "8032": 55
+            },
+            "Letras": {
+                "8022": 12,
+            },
+            "Portaria III": {
+                "8012": 49,
+                "8022": 67
+            },
+            "Biblioteca Farmácia e Química": {
+                "8022": 156
+            },
+            "Odontologia": {
+                "8012": 14
+            },
+            "Poli Biênio": {
+                "8032": 80,
+                "8012": 46
+            },
+            "Terminal Urbano": {
+                "8022": 19
+            },
+            "IPT": {
+                "8022": 19,
+            },
+            "Poli Civil": {
+                "8032": 19
+            },
+            "Poli Eletrotécnica": {
+                "8022": 94,
+            },
+            "Parada Matemática": {
+                "8012": 14,
+                "8022": 27
+            },
+            "Rua do Lago": {
+                "8022": 14
+            },
+            "Acesso CPTM I": {
+                "8022": 9
+            },
+            "Hospital Universitário": {
+                "8012": 405,
+                "8022": 176
+            },
+            "MAE": {
+                "8012": 58,
+            },
+            "FAU II": {
+                "8022": 37
+            },
+            "Cocesp I": {
+                "8012": 47,
+            },
+            "Raia Olímpica": {
+                "8022": 156,
+            },
+            "CRUSP": {
+                "8032": 31
+            },
+            "Reitoria": {
+                "8012": 93
+            },
+            "COPESP": {
+                "8022": 31
+            }
+        },
+         1080: {
+            "FEA": {
+                "8012": 35,
+                "8032": 13,
+            },
+            "Portaria III": {
+                "8012": 56,
+                "8022": 92
+            },
+            "Biblioteca Farmácia e Química": {
+                "8022": 71
+            },
+            "Poli Biênio": {
+                "8032": 71,
+                "8012": 29
+            },
+            "Odontologia": {
+                "8012": 14
+            },
+            "IPT": {
+                "8022": 18,
+            },
+            "Poli Civil": {
+                "8032": 18
+            },
+            "Butantan": {
+                "8022": 18
+            },
+            "Parada Matemática": {
+                "8012": 15,
+                "8022": 15
+            },
+            "Rua do Lago": {
+                "8022": 15
+            },
+            "Acesso CPTM I": {
+                "8022": 9
+            },
+            "Hospital Universitário": {
+                "8012": 508,
+                "8022": 160
+            },
+            "Reitoria/Bancos": {
+                "8012": 16,
+                "8032": 37
+            },
+            "MAE": {
+                "8012": 29
+            },
+            "Poli Eletrotécnica": {
+                "8022": 11
+            },
+            "FAU II": {
+                "8022": 12
+            },
+            "Cocesp I": {
+                "8012": 37
+            },
+            "Raia Olímpica": {
+                "8022": 73,
+            },
+            "CRUSP": {
+                "8032": 37
+            },
+            "Biblioteca Brasiliana": {
+                "8012": 15,
+                "8032": 52
+            },
+            "Reitoria": {
+                "8012": 37
+            }
+        }
+    }
+}
---
+++ pontos_ajustados_volta_p3.py
@@ -0,0 +1,339 @@
+# dados filtrados do arquivo volta_aluno_p3_18h.json
+
+demanda_completa_volta_p3 = {
+    "sex": {
+        1110: {
+            "História e Geografia": {
+                "8022": 342
+            },
+            "Letras": {
+                "8022": 301
+            },
+            "FAU II": {
+                "8022": 403
+            },
+            "Biblioteca Farmácia e Química": {
+                "8022": 152
+            },
+            "Reitoria": {
+                "8012": 137
+            },
+            "FEA": {
+                "8012": 388,
+            },
+            "Poli Biênio": {
+                "8012": 133,
+            },
+            "Terminal Urbano": {
+                "8022": 61
+            },
+            "Raia Olímpica": {
+                "8022": 75
+            },
+            "IPT": {
+                "8022": 15
+            },
+            "Portaria II": {
+                "8022": 15
+            },
+            "Farmácia e Química": {
+                "8022": 26
+            },
+            "Acesso Vl. Indiana": {
+                "8012": 101
+            },
+            "Reitoria/Bancos": {
+                "8012": 304,
+            },
+            "Geociências": {
+                "8022": 25
+            },
+            "Odontologia": {
+                "8022": 111
+            },
+            "Cocesp I": {
+                "8012": 80
+            },
+            "Rua do Lago": {
+                "8022": 19
+            },
+            "Educação": {
+                "8012": 19
+            },
+            "Biomédicas III": {
+                "8022": 19
+            },
+            "Poli Eletrotécnica": {
+                "8022": 104
+            }
+        }
+    },
+    "ter": {
+        1110: {
+            "Reitoria/Bancos": {
+                "8012": 263,
+            },
+            "Geociências": {
+                "8022": 43
+            },
+            "História e Geografia": {
+                "8022": 660
+            },
+            "Poli Biênio": {
+                "8012": 180,
+            },
+            "Terminal Urbano": {
+                "8022": 78
+            },
+            "Raia Olímpica": {
+                "8022": 121
+            },
+            "Biblioteca Farmácia e Química": {
+                "8022": 217
+            },
+            "FEA": {
+                "8012": 565
+            },
+            "IPT": {
+                "8022": 19
+            },
+            "Portaria II": {
+                "8022": 19
+            },
+            "Poli Eletrotécnica": {
+                "8022": 174
+            },
+            "Letras": {
+                "8022": 558
+            },
+            "Rua do Lago": {
+                "8022": 41
+            },
+            "Educação": {
+                "8012": 41
+            },
+            "Biomédicas III": {
+                "8022": 41
+            },
+            "Farmácia e Química": {
+                "8022": 40
+            },
+            "FAU II": {
+                "8022": 585
+            },
+            "Odontologia": {
+                "8022": 104
+            },
+            "Acesso Vl. Indiana": {
+                "8012": 96
+            },
+            "Reitoria": {
+                "8012": 361
+            },
+            "Cocesp I": {
+                "8012": 72
+            }
+        }
+    },
+    "qui": {
+        1110: {
+            "Acesso Vl. Indiana": {
+                "8012": 136
+            },
+            "Reitoria/Bancos": {
+                "8012": 224,
+            },
+            "Geociências": {
+                "8022": 42
+            },
+            "Raia Olímpica": {
+                "8022": 111
+            },
+            "Biblioteca Farmácia e Química": {
+                "8022": 178
+            },
+            "Letras": {
+                "8022": 564
+            },
+            "História e Geografia": {
+                "8022": 650
+            },
+            "FAU II": {
+                "8022": 477
+            },
+            "FEA": {
+                "8012": 459,
+            },
+            "Rua do Lago": {
+                "8022": 46
+            },
+            "Educação": {
+                "8012": 46
+            },
+            "Biomédicas III": {
+                "8022": 46
+            },
+            "Poli Biênio": {
+                "8012": 188,
+            },
+            "Terminal Urbano": {
+                "8022": 72
+            },
+            "IPT": {
+                "8022": 18
+            },
+            "Portaria II": {
+                "8022": 18
+            },
+            "Farmácia e Química": {
+                "8022": 16
+            },
+            "Odontologia": {
+                "8022": 221
+            },
+            "Poli Eletrotécnica": {
+                "8022": 206
+            },
+            "Cocesp I": {
+                "8012": 150
+            },
+            "Reitoria": {
+                "8012": 258
+            }
+        }
+    },
+    "seg": {
+        1110: {
+            "História e Geografia": {
+                "8022": 546
+            },
+            "Poli Biênio": {
+                "8012": 197,
+            },
+            "Terminal Urbano": {
+                "8022": 85
+            },
+            "Raia Olímpica": {
+                "8022": 112
+            },
+            "Biblioteca Farmácia e Química": {
+                "8022": 197
+            },
+            "FEA": {
+                "8012": 510
+            },
+            "IPT": {
+                "8022": 21
+            },
+            "Portaria II": {
+                "8022": 21
+            },
+            "Letras": {
+                "8022": 464
+            },
+            "Rua do Lago": {
+                "8022": 39
+            },
+            "Educação": {
+                "8012": 39
+            },
+            "Biomédicas III": {
+                "8022": 39
+            },
+            "Poli Eletrotécnica": {
+                "8022": 183
+            },
+            "FAU II": {
+                "8022": 531
+            },
+            "Reitoria": {
+                "8012": 258
+            },
+            "Reitoria/Bancos": {
+                "8012": 352,
+            },
+            "Geociências": {
+                "8022": 22
+            },
+            "Acesso Vl. Indiana": {
+                "8012": 114
+            },
+            "Cocesp I": {
+                "8012": 134
+            },
+            "Odontologia": {
+                "8022": 205
+            },
+            "Farmácia e Química": {
+                "8022": 38
+            }
+        }
+    },
+    "qua": {
+        1110: {
+            "FAU II": {
+                "8022": 485
+            },
+            "FEA": {
+                "8012": 463,
+            },
+            "Acesso Vl. Indiana": {
+                "8012": 148
+            },
+            "Poli Eletrotécnica": {
+                "8022": 193,
+            },
+            "Poli Biênio": {
+                "8012": 208
+            },
+            "História e Geografia": {
+                "8022": 451
+            },
+            "Letras": {
+                "8022": 388
+            },
+            "Reitoria/Bancos": {
+                "8012": 306,
+            },
+            "Geociências": {
+                "8022": 39
+            },
+            "Raia Olímpica": {
+                "8022": 124
+            },
+            "Biblioteca Farmácia e Química": {
+                "8022": 196
+            },
+            "Reitoria": {
+                "8012": 324
+            },
+            "Terminal Urbano": {
+                "8022": 87
+            },
+            "IPT": {
+                "8022": 22
+            },
+            "Portaria II": {
+                "8022": 22
+            },
+            "Cocesp I": {
+                "8012": 150
+            },
+            "Odontologia": {
+                "8022": 218
+            },
+            "Farmácia e Química": {
+                "8022": 30
+            },
+            "Rua do Lago": {
+                "8022": 46
+            },
+            "Educação": {
+                "8012": 46
+            },
+            "Biomédicas III": {
+                "8022": 46
+            }
+        }
+    }
+}
