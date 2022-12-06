a86737fe1a139e021e5a7f7246d076b449ee872e
Luca Dillenburg <luca.assumpcao.dillenburg@gmail.com>
2022-12-06 20:59:45 -0300

    Chamar simulacao na API criada

--- server.py
+++ server.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
+from verification_demand import simula
+from verification_demand import cria_linhas_sptrans
 
 app = FastAPI()
 
 @app.post("/")
 async def simulate_buses():
-    return {"Hello": "World"}
+    total_ida_manha, total_ida_tarde, total_volta_tarde, porc_atendimento_ida, porc_atendimento_volta = simula(cria_linhas_sptrans())
+
+    # média de porcentagens da demanda entregue por ponto
+    return {
+        'totais': {
+            'ida-manha': total_ida_manha,
+            'ida-tarde': total_ida_tarde,
+            'volta-tarde': total_volta_tarde,
+        },
+        'ida': porc_atendimento_ida,
+        'volta': porc_atendimento_volta
+    }
