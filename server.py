e4cca876145c0ddb82107344e2b65c472af96504
José Lucas Silva Mayer <josemayer@usp.br>
2022-12-19 17:06:59 -0400

    Garante permissões de requisições de todas as origens ao servidor
    Signed-off-by: José Lucas Silva Mayer <josemayer@usp.br>

--- server.py
+++ server.py
@@ -1,10 +1,21 @@
 import json
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from typing import Dict, Any
 from verification_demand import LinhaRota, Demanda, Rota, simula, junta_demanda
 from verification import MediaPercurso, Linha, calcular_horarios_saidas
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/")
 async def simulate_buses(body: Dict[Any, Any]):
